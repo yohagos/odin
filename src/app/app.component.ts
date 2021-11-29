e5b1afd3d91380194f445e45a2b869ce8e5bb1c0
yohagos <yosef.hagos@googlemail.com>
2021-11-29 14:30:01 +0100

    working on the service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output } from '@angular/core';
+import { NameService } from './name.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers: [NameService]
 })
 export class AppComponent {
-  title = 'odin';
+  public text: string = "";
+
+  public responses: any;
+
+  constructor(public nameService: NameService) { }
+
+  public service() {
+    console.log(this.text);
+    this.nameService.post(this.text).subscribe(
+      (Response) => {
+        console.log(Response.toString);
+      }
+    )
+  }
 }
---
+++ src/app/name.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams, HttpHeaders } from "@angular/common/http";
+import { environment } from 'src/environments/environment';
+import { map, tap } from 'rxjs';
+
+@Injectable()
+export class NameService {
+
+  public xyz: any;
+
+  constructor(public http: HttpClient) { }
+
+  httpHeader = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json'
+    })
+  }
+
+  public get(message: string) {
+    let params = new HttpParams();
+    params = params.append("name", message);
+
+    return this.http.get(environment.server + "/name", {observe: "response", params}).pipe(
+     map(response => {
+      return this.xyz = response.body
+     })
+    )
+  }
+
+  public post(message: string) {
+    console.log(message)
+    console.log(environment.server)
+   return this.http.post(environment.server + "/name", {name: message}, this.httpHeader).pipe(
+      tap(data => {
+        console.log("pipe tap :" + data);
+      })
+    )
+  }
+}
